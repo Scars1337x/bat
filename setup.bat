48901b4e5aabb8cbce5a9b04f0ced6681d59fef7
Scars1337x <88856691+Scars1337x@users.noreply.github.com>
2021-11-13 14:20:35 +0000

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,91 @@
+@echo off
+title Game Setup.
+echo. 
+echo. 
+echo. 
+echo. 
+echo                                                             ===============================================
+echo                                                Setting game up. May close a few apps to free up memory for the installation.
+echo                                                             ===============================================
+echo. 
+echo. 
+echo. 
+echo. 
+color 04
+echo ==========================
+echo Setup 05 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 10 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 15 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 20 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 25 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 30 Percent Complete.
+echo ==========================
+timeout /t 1 /nobreak > NUL
+echo. 
+cd Masked Horror/Setup/Setup Log
+setup.txt
+set /a number=%random% %% 100 +503
+echo                                                                         =======================
+echo                                                      Discord.exe has been restarted to free up memory for installation.
+echo                                                                   %number%MB Of memory has been cleared.
+echo                                                                         =======================
+echo. 
+echo ==========================
+echo Setup 35 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 40 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 45 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 50 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 55 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 60 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 65 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 70 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 75 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 80 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 85 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 90 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 95 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Setup 100 Percent Complete.
+echo ==========================
+timeout /t 1 /nobreak > NUL
+echo. 
+echo                                                                                   ======
+echo                                                                                 Installing...
+echo                                                                                   ======
+echo. 
+timeout /t 1 /nobreak > NUL
+echo ==========================
+echo Installing 99 Percent Complete.
+timeout /t 1 /nobreak > NUL
+echo Installing 100% Complete.
+echo ==========================
+echo. 
+timeout /t 1 /nobreak > NUL
+echo                                                                                   ======
+echo                                                                            Installation COMPLETE.
+echo                                                                                   ======
+echo.
+timeout /t 1 /nobreak > NUL
+echo                                                                      ================================
+echo                                                            Please open 'Masked Horror' Located in this folder.
+echo                                                                      ================================
+timeout /t 1 /nobreak > NUL
+echo. 
+pause
